d79373a444c670d8b3f5438585654f3350baefa3
Ladace <ladacez@gmail.com>
2014-06-28 23:28:43 +0800

    new test

---
+++ Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	private string touching = "";
+	
+	void Update () {
+		transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 4, Input.GetAxis("Vertical") * Time.deltaTime * 4);
+		touching = "";
+	}
+
+	void OnCollide (CollisionInfo info) {
+		touching += info.normal.ToString();
+	}
+
+	void OnGUI () {
+		GUI.Box(new Rect(0, 0, Screen.width, 40), touching);
+	}
+
+}
